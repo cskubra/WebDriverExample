e6ae323a2a39f205bd9c40975b6bb3a23bf97b54
Kubra <cskubra@gmail.com>
2023-03-04 17:25:19 +0300

    Add project files.

---
+++ WebDriverExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebDriverExample
+{
+    internal class Program
+    {
+
+        //1.	Open the Google Chrome browser.
+        //2.	Navigate to the BBC homepage.
+        //3.	Maximize window
+        //4.    Find "Sport" then click 
+        //5.	Close the browser.
+        static void Main(string[] args)
+        {
+
+            Console.Write("test case started ");
+            //1
+            IWebDriver driver = new ChromeDriver(@"D:\WebDriver\Chrome");
+            //2
+            driver.Navigate().GoToUrl("https://www.bbc.com/");
+            //3
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+
+            //4
+            IWebElement ele = driver.FindElement(By.XPath("(//span[text()='Sport'])[2]"));
+            ele.Click();
+            Thread.Sleep(3000);
+
+            //5
+            driver.Close();
+            Console.Write("test case ended ");
+        }
+    }
+}
---
+++ WebDriverExample/TestExample.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
+
+namespace WebDriverExample
+{
+    class TestExample
+    {
+        IWebDriver driver = new ChromeDriver(@"D:\WebDriver\Chrome");
+
+        [SetUp]
+        public void Initialize()
+        {
+            //navigate to URL  
+            driver.Navigate().GoToUrl("http://www.ilkumut.biz/");
+
+            //Maximize the browser window  
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(3000);
+
+
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //find iletisim button and click  
+            IWebElement iletisim = driver.FindElement(By.XPath("//a[@href='iletisim.aspx']"));
+            iletisim.Click();
+
+            //write name and surname
+            IWebElement nameSurname = driver.FindElement(By.Id("ContentPlaceHolder1_txtadi"));
+            nameSurname.SendKeys("Kubra Caliskan");
+            //write email
+            IWebElement eMail = driver.FindElement(By.Id("ContentPlaceHolder1_txtmail"));
+            eMail.SendKeys("deneme@test.com");
+            //write subject
+            IWebElement konu = driver.FindElement(By.Id("ContentPlaceHolder1_txtkonu"));
+            konu.SendKeys("Deneme Mesajı");
+            //write message
+            IWebElement mesaj = driver.FindElement(By.Id("ContentPlaceHolder1_txtmesaj"));
+            mesaj.SendKeys("Deneme Mesajı");
+                       
+            //click on the send button  
+            IWebElement gonder = driver.FindElement(By.Id("ContentPlaceHolder1_btngonder"));
+            gonder.Click();
+            Thread.Sleep(3000);
+            //click close button after message send successfully
+            IWebElement kapat = driver.FindElement(By.Id("ContentPlaceHolder1_WebMessageBox_btnClose"));
+            kapat.Click();
+            
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            //close the browser  
+            driver.Close();
+        }
+    }
+}
